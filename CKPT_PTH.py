eb78ad06946fc466eaec9e480e3e722f3554555c
Nikolai Shangin <shanginn@gmail.com>
2024-03-11 11:54:59 +0700

    Refactor CKPT_PTH.py and SUPIR_v0_Juggernautv9_lightning.yaml files.

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,7 +1,7 @@
-from download_weights import CACHE_DIR
+# from download_weights import CACHE_DIR
 
-LLAVA_CLIP_PATH = f'{CACHE_DIR}/AIGC_pretrain/LLaVA1.5/clip-vit-large-patch14-336'
-LLAVA_MODEL_PATH = f'{CACHE_DIR}/AIGC_pretrain/LLaVA1.5/llava-v1.5-13b'
-SDXL_CLIP1_PATH = f'{CACHE_DIR}/AIGC_pretrain/clip-vit-large-patch14'
-# SDXL_CLIP2_CKPT_PTH = f'{CACHE_DIR}/AIGC_pretrain/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
-SDXL_CLIP2_CKPT_PTH = None
+# LLAVA_CLIP_PATH, SDXL_CLIP1_PATH, SDXL_CLIP2_CKPT_PTH
+LLAVA_CLIP_PATH = None  # f'{CACHE_DIR}/AIGC_pretrain/LLaVA1.5/clip-vit-large-patch14-336'
+LLAVA_MODEL_PATH = None  # f'{CACHE_DIR}/AIGC_pretrain/LLaVA1.5/llava-v1.5-13b'
+SDXL_CLIP1_PATH = None  # f'{CACHE_DIR}/AIGC_pretrain/clip-vit-large-patch14'
+SDXL_CLIP2_CKPT_PTH = None  # f'{CACHE_DIR}/AIGC_pretrain/CLIP-ViT-bigG-14-laion2B-39B-b160k'
